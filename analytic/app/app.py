94b29e1a3087a0c7f6ef89cb13c3da2510d109ea
efrainlaycastillo <efrainlaycastillo@hotmail.com>
2021-03-21 23:31:02 -0500

    python app REST getting data from mongodb

---
+++ analytic/app/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify, Response
+from flask_pymongo import PyMongo
+from bson import json_util
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/bank"
+mongo = PyMongo(app)
+
+@app.route('/users/<id>', methods=['GET'])
+def kmeans(id):
+    print(id)
+    data = mongo.db.clients.find({"CLIENTNUM": int(id)})
+    response = json_util.dumps(data)
+
+    return Response(response, mimetype='application/json')
+
+@app.errorhandler(404)
+def not_found(error=None):
+    response = jsonify({
+        'message':'No se encontro el recurso' + request.url,
+        'status' : 404
+    })
+    response.status_code = 404
+    return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ analytic/readme.md
@@ -0,0 +1,6 @@
+python3 -m pip install whatever
+
+https://gist.github.com/Geoyi/d9fab4f609e9f75941946be45000632b
+
+https://flask-pymongo.readthedocs.io/en/latest/
+
